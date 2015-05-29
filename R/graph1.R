4c4a1e2ab8c13a4f44aea2155218b2fc532fec7d
johanez <johannes.eberenz@gmai.com>
2015-05-29 21:51:54 +0200

    ghpages

---
+++ R/graph1.R
@@ -0,0 +1,31 @@
+require(d3Network)
+require(networkD3)
+
+# Load data
+data(MisLinks)
+data(MisNodes)
+
+
+
+nodes <- read.csv("nodes1.csv", header = T)
+links <- read.csv("links.csv", header = T)
+links[,1:2] <- links[,1:2]-1
+links$value <- 1
+
+# Create graph
+forceNetwork(Links = links, Nodes = nodes, Source = "source", charge=-100,
+             Target = "target", Value = "value", NodeID = "name",
+             Group = "group", opacity = 0.8, linkDistance = "function(d){return 10- Math.sqrt(d.value)}")
+
+nodes <- data.frame(id=c(1,2), group=1)
+links <- data.frame(source=0, target=1, value=3)
+
+
+# Load data
+data(MisLinks)
+data(MisNodes)
+
+# Create graph
+forceNetwork(Links = MisLinks, Nodes = MisNodes, Source = "source",
+             Target = "target", Value = "value", NodeID = "name",
+             Group = "group", opacity = 0.4)
